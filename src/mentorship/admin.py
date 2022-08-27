c32c2620411390146a8d0f61af604c59ff3e8f95
alsaheem <adebisiayomide07@gmail.com>
2022-08-27 14:40:42 +0100

    added logic to dashboard

--- src/mentorship/admin.py
+++ src/mentorship/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mentorship.models import SessionReview,Scheduler,MentorshipSession
+
 # Register your models here.
+admin.site.register(SessionReview)
+admin.site.register(Scheduler)
+admin.site.register(MentorshipSession)
